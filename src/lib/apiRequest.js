4fa97e50603192a224d987ff91c6d28edf484039
alluri-reddy <136157062+alluri-reddy@users.noreply.github.com>
2024-04-28 18:26:23 +0530

    Update apiRequest.js

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,6 +2,25 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: "https://estate-backend-theta.vercel.app/api",
+  withCredentials: true,
+});
+
+// Function to get JWT token from localStorage
+const getToken = () => {
+  return localStorage.getItem("jwtToken");
+};
+
+// Request interceptor to add JWT token to each request
+apiRequest.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
 });
 
 export default apiRequest;
